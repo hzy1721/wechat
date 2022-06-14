5db77024653204cbe364cec71f8c0711a7a577ef
hzy1721 <hzy1721@qq.com>
2022-06-14 15:27:42 +0800

    Navigation guard of home/login

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import App from './App.vue'
 import router from './router'
 import 'normalize.css/normalize.css'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import 'element-plus/dist/index.css'
 
 const app = createApp(App)
 
--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 import LoginView from '@/views/login/index.vue';
 import HomeView from '@/views/home/index.vue';
 import RegisterView from '@/views/register/index.vue';
+import { ElMessage } from 'element-plus';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,11 +11,29 @@ const router = createRouter({
       path: '/login',
       name: 'login',
       component: LoginView,
+      beforeEnter: () => {
+        if (sessionStorage.getItem('token')) {
+          ElMessage({
+            message: '您已登陆，跳转到首页',
+            type: 'success'
+          });
+          return '/';
+        }
+      }
     },
     {
       path: '/',
       name: 'home',
       component: HomeView,
+      beforeEnter: () => {
+        if (!sessionStorage.getItem('token')) {
+          ElMessage({
+            message: '请先登录',
+            type: 'warning'
+          });
+          return '/login';
+        }
+      }
     },
     {
       path: '/register',
